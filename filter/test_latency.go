9b2bbb005220907f37a4a49d494531172ec2ece1
Stitch-Zhang <zl936226084@gmail.com>
2021-04-11 21:14:18 +0800

    feat:新加结果排序方法

--- filter/test_latency.go
+++ filter/test_latency.go
@@ -2,6 +2,7 @@ package filter
 
 import (
 	"errors"
+	"net"
 	"os"
 	"strings"
 	"sync"
@@ -20,12 +21,29 @@ type Result struct {
 	Error error
 }
 
+// Results for results
+type Results []Result
+
+// SortByDuration just sort follow duration
+// Using insert sort
+func (r *Results) SortByDuration() {
+	re := insertSort(*r)
+	r = &re
+}
+
 // LatencyTestWithChan test latency between remote and local,
 // and store valid remote's index in c
 // the faster the less for remote's index
 //
+// Args:
+// addrs -> []string{"www.bing.com","192.168.12.1"}
+// c 	 -> store result for goroutine
+// loops -> ping times
+// retry -> wheather try to use net.Dial to test latency after ping failed
+// timeout -> timeout for ping and net.Dial
+//
 // this is based on icmp protocl, requiring to run as privileged otherwise block
-func LatencyTestWithChan(addrs []string, c chan<- Result, loops int, timeout time.Duration) error {
+func LatencyTestWithChan(addrs []string, c chan<- Result, loops int, retry bool, timeout time.Duration) error {
 	if len(addrs) < 1 {
 		return errors.New("remote host addr must more than one")
 	}
@@ -41,16 +59,22 @@ func LatencyTestWithChan(addrs []string, c chan<- Result, loops int, timeout tim
 		go func(addr string, idx int) {
 			defer wg.Done()
 			var err error
+			var latency time.Duration
 			// By using icmp(ping) to test latency
 			p := ping.New(addr)
 			p.Timeout = timeout
 			p.Count = loops
 			err = p.Run()
 			statis := p.Statistics()
-			if statis.PacketLoss == 100 {
-				err = errors.New(addr + ":packet loss reach 100%")
+			// handle remote server banned icmp(ping)
+			// switch tp tcp:http
+			if statis.PacketLoss == 100 && retry {
+				last := time.Now()
+				_, err = net.DialTimeout("tcp", addr+":http", timeout)
+				latency = time.Now().Sub(last)
+			} else {
+				latency = statis.AvgRtt
 			}
-			latency := statis.AvgRtt
 			c <- Result{Address: addr, Latency: latency, Error: err}
 		}(addr, idx)
 	}
@@ -62,10 +86,16 @@ func LatencyTestWithChan(addrs []string, c chan<- Result, loops int, timeout tim
 // and store valid remote's index in c
 // tresult are stored in rets,the faster remote host the less index are
 //
-// for improving efficiency to use LatencyTestWithChan in goroutine instead of this
+// Args:
+// addrs -> []string{"www.bing.com","192.168.12.1"}
+// loops -> ping times
+// retry -> wheather try to use net.Dial to test latency after ping failed
+// timeout -> timeout for ping and net.Dial
 //
+// for improving efficiency to use LatencyTestWithChan in goroutine instead of this
+// but this provide a sort method(Results.SortByDuration)
 // This is based on icmp protocl, requiring to run as privileged otherwise block
-func LatencyTest(addrs []string, loops int, timeout time.Duration) (rets []Result, err error) {
+func LatencyTest(addrs []string, loops int, retry bool, timeout time.Duration) (rets Results, err error) {
 	if len(addrs) < 1 {
 		return nil, errors.New("remote host addr must more than one")
 	}
@@ -81,15 +111,21 @@ func LatencyTest(addrs []string, loops int, timeout time.Duration) (rets []Resul
 		go func(addr string, idx int) {
 			defer wg.Done()
 			var err error
+			var latency time.Duration
 			p := ping.New(addr)
 			p.Timeout = timeout
 			p.Count = loops
 			err = p.Run()
 			statis := p.Statistics()
-			if statis.PacketLoss == 100 {
-				err = errors.New(addr + ":packet loss reach 100%")
+			// handle remote server banned icmp(ping)
+			// switch tp tcp:http
+			if statis.PacketLoss == 100 && retry {
+				last := time.Now()
+				_, err = net.DialTimeout("tcp", addr+":http", timeout)
+				latency = time.Now().Sub(last)
+			} else {
+				latency = statis.AvgRtt
 			}
-			latency := statis.AvgRtt
 			rets = append(rets, Result{Address: addr, Latency: latency, Error: err})
 		}(addr, idx)
 	}
@@ -106,4 +142,18 @@ func urlHandler(urls []string) (validURLs []string) {
 		validURLs = append(validURLs, s)
 	}
 	return
+}
+
+func insertSort(res Results) Results {
+	for i := 1; i < len(res); i++ {
+		tmp := res[i]
+		j := i - 1
+		for j >= 0 && tmp.Latency < res[j].Latency {
+			res[j+1] = res[j]
+			j--
+		}
+		res[j+1] = tmp
+	}
+
+	return res
 }
--- filter/test_latency_test.go
+++ filter/test_latency_test.go
@@ -13,13 +13,23 @@ import (
 func TestSpeedTest(t *testing.T) {
 	var wg sync.WaitGroup
 	urls := []string{
-		"mirrors.tuna.tsinghua.edu.cn",
-		"https://www.baidu.com",
-		"www.google.com",
-		"www.da.cc",
-		"123.22.33.200",
-		"tsing.mirrors.com/ubuntu/",
-		"www",
+		"https://mirrors.huaweicloud.com/",
+		"https://mirrors.cloud.tencent.com/",
+		"http://mirrors.163.com/",
+		"http://mirrors.sohu.com/",
+		"http://mirrors.yun-idc.com/",
+		"https://mirrors.tuna.tsinghua.edu.cn/",
+		"http://mirrors.ustc.edu.cn/",
+		"http://mirror.neu.edu.cn/",
+		"https://mirror.bjtu.edu.cn/",
+		"http://mirrors.zju.edu.cn/",
+		"http://mirrors.hust.edu.cn/",
+		"https://mirrors.njupt.edu.cn/",
+		"http://mirrors.neusoft.edu.cn/",
+		"http://mirrors.hit.edu.cn/#/home",
+		"https://mirrors.nju.edu.cn/",
+		"http://mirrors.cqu.edu.cn/",
+		"https://mirrors.dgut.edu.cn/",
 	}
 	c := make(chan Result, 1)
 	go func() {
@@ -34,7 +44,7 @@ func TestSpeedTest(t *testing.T) {
 			}
 		}
 	}()
-	errs := LatencyTestWithChan(urls, c, 5, 5*time.Second)
+	errs := LatencyTestWithChan(urls, c, 5, true, 5*time.Second)
 	if errs != nil {
 
 	}
@@ -44,21 +54,29 @@ func TestSpeedTest(t *testing.T) {
 
 func TestLatencyTest(t *testing.T) {
 	urls := []string{
-		"mirrors.tuna.tsinghua.edu.cn",
-		"https://www.baidu.com",
-		"www.google.com",
-		"www.da.cc",
-		"123.22.33.200",
-		"8.8.8.8",
-		"tsing.mirrors.com/ubuntu/",
-		"www",
+		"https://mirrors.huaweicloud.com/",
+		"https://mirrors.cloud.tencent.com/",
+		"http://mirrors.163.com/",
+		"http://mirrors.sohu.com/",
+		"http://mirrors.yun-idc.com/",
 		"https://mirrors.tuna.tsinghua.edu.cn/",
-		"one.pantacor.com",
-		"p.hjjjh.com",
+		"http://mirrors.ustc.edu.cn/",
+		"http://mirror.neu.edu.cn/",
+		"https://mirror.bjtu.edu.cn/",
+		"http://mirrors.zju.edu.cn/",
+		"http://mirrors.hust.edu.cn/",
+		"https://mirrors.njupt.edu.cn/",
+		"http://mirrors.neusoft.edu.cn/",
+		"http://mirrors.hit.edu.cn/#/home",
+		"https://mirrors.nju.edu.cn/",
+		"http://mirrors.cqu.edu.cn/",
+		"https://mirrors.dgut.edu.cn/",
 	}
-	results, err := LatencyTest(urls, 5, 5*time.Second)
+	results, err := LatencyTest(urls, 5, true, 5*time.Second)
+	results.SortByDuration()
 	if err != nil {
-		log.Panicln(err)
+		log.Println(err)
+		return
 	}
 	for _, v := range results {
 		if v.Error != nil {
