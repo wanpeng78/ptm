c06a0f8e4295d10fd44b32f80a8dce48cf5611e5
Stitch-Zhang <zl936226084@gmail.com>
2021-04-19 13:40:08 +0800

    feat:v0.1发布

--- cmd/ptm/main.go
+++ cmd/ptm/main.go
@@ -1,41 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
 
 	"gitee.com/Stitchtor/ptm/filter"
+	"gitee.com/Stitchtor/ptm/pkgm"
 	"gitee.com/Stitchtor/ptm/syscheck"
 	colorful "github.com/fatih/color"
 	"github.com/jedib0t/go-pretty/v6/table"
-	"github.com/lorenzosaino/go-sysctl"
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	// Check privileged
-	if os.Geteuid() != 0 {
-		colorful.Red("暂不支持非root用户使用,请以root权限运行")
-		os.Exit(0)
-	}
-	// set ping privileged
-	err := enablePing()
-	if err != nil {
-		colorful.Red("ping权限设置失败 %s", err.Error())
-		os.Exit(0)
-	}
-	// Check update and fetch mirror source
-	mirrorSource, err := fetchData()
-	if err != nil {
-		colorful.Red("无法从远程获取镜像数据:%s", err.Error())
-	}
-	if err != nil {
-		colorful.Red("%s\n", err.Error())
-	}
+
 	cfg := &Config{}
 	app := &cli.App{
 		Name:      "ptm",
@@ -43,17 +27,19 @@ func main() {
 		Copyright: "Copyright (c) " + strconv.Itoa(time.Now().Year()) + " Stitch-Zhang",
 		Version:   fmt.Sprintf("%.2f", version),
 		Flags:     cliParse(cfg),
-		Action:    run(cfg, mirrorSource),
+		Action:    run(cfg),
 	}
 	app.UseShortOptionHandling = true
 	app.HideHelpCommand = true
+	sort.Sort(cli.FlagsByName(app.Flags))
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func run(cfg *Config, ms *MirrorsOnline) cli.ActionFunc {
+func run(cfg *Config) cli.ActionFunc {
 	return func(c *cli.Context) error {
+		ms := checkAndFetch(cfg.APIURL)
 		osinfo, err := syscheck.SystemInfo()
 		if err != nil {
 			return err
@@ -71,14 +57,37 @@ func run(cfg *Config, ms *MirrorsOnline) cli.ActionFunc {
 			colorful.Red("延迟测试失败")
 			return nil
 		}
+		// choice mirrors
 		mirrorsInfo.SortByDuration()
-		showMirrorsInfo(mirrorsInfo, false)
+		mirrorsIdx := showMirrorsInfo(mirrorsInfo, *cfg)
+
+		// translate mirror index to mirror
+		targetMirrors := make(filter.Results, 0)
+		for _, idx := range mirrorsIdx {
+			targetMirrors = append(targetMirrors, mirrorsInfo[idx])
+		}
+
+		// prepare to write
+		pm, err := pkgm.NewPKGM()
+		if err != nil {
+			colorful.Red("实例化包管理失败：%s\n", err.Error())
+			os.Exit(0)
+		}
+		err = pm.WriteFile(targetMirrors)
+		if err != nil {
+			colorful.Red("写入镜像文件失败：%s\n", err.Error())
+			os.Exit(0)
+		}
+		colorful.Green("写入成功，正在清理并更新镜像缓存")
+		reader := pm.Refresh()
+		displayRefresh(reader)
+		colorful.Green("Everything is done!\n")
 		return nil
 
 	}
 }
 
-func showMirrorsInfo(mirrors filter.Results, onlyShow bool) []int {
+func showMirrorsInfo(mirrors filter.Results, cfg Config) (choices []int) {
 	mirrorsTable := table.NewWriter()
 	choicesTable := table.NewWriter()
 	mirrorsTable.AppendHeader(table.Row{"#", "镜像名", "延迟(ms)", "地址", "备注"})
@@ -91,40 +100,75 @@ func showMirrorsInfo(mirrors filter.Results, onlyShow bool) []int {
 		}
 	}
 	fmt.Println(mirrorsTable.Render())
-	if onlyShow {
-		return nil
+
+	// AutoConfig enable
+	if cfg.AutoConfig {
+		for idx, mirror := range mirrors {
+			if mirror.Ping {
+				choices = append(choices, idx)
+			}
+			if len(choices) >= int(cfg.MirrorCounts) {
+				return
+			}
+		}
 	}
-	var choicestr string
-	var choices []int
-	appended := make(map[int]bool)
-	fmt.Print("请输入所需要镜像站的序号(以,分割):")
-	fmt.Scanf("%s", &choicestr)
-	strs := strings.Split(choicestr, ",")
-	for _, v := range strs {
-		idx, err := strconv.Atoi(v)
-		if err != nil || idx > len(mirrors) {
-			colorful.Red("序号错误:%d\n", idx)
-			continue
+	//Manual config mirrors
+	choice := func() {
+		var choicestr string
+		appended := make(map[int]bool)
+		fmt.Print("请输入所需要镜像站的序号(以,分割):")
+		fmt.Scanf("%s", &choicestr)
+		strs := strings.Split(choicestr, ",")
+		for _, v := range strs {
+			idx, err := strconv.Atoi(v)
+			if err != nil || idx > len(mirrors) || idx < 0 {
+				colorful.Red("序号错误:%d\n", idx)
+				continue
+			}
+			if appended[idx] {
+				colorful.Red("序号重复:%d", idx)
+				continue
+			}
+			appended[idx] = true
+			choices = append(choices, idx)
+			if mirrors[idx].Ping {
+				choicesTable.AppendRow(table.Row{idx, mirrors[idx].Name, mirrors[idx].Latency.Milliseconds(), mirrors[idx].Address})
+			} else {
+				choicesTable.AppendRow(table.Row{idx, mirrors[idx].Name, mirrors[idx].Latency.Milliseconds(), mirrors[idx].Address, "远程服务器禁止ping，请以访问延迟为准"})
+			}
 		}
-		if appended[idx] {
-			colorful.Red("序号重复:%d", idx)
-			continue
-		}
-		appended[idx] = true
-		choices = append(choices, idx)
-		if mirrors[idx].Ping {
-			choicesTable.AppendRow(table.Row{idx, mirrors[idx].Name, mirrors[idx].Latency.Milliseconds(), mirrors[idx].Address})
-		} else {
-			choicesTable.AppendRow(table.Row{idx, mirrors[idx].Name, mirrors[idx].Latency.Milliseconds(), mirrors[idx].Address, "远程服务器禁止ping，请以访问延迟为准"})
-		}
+	}
+	for len(choices) == 0 {
+		choice()
 	}
 	colorful.Green("已选的镜像:\n")
 	fmt.Println(choicesTable.Render())
 	return choices
 }
 
-// enable ipv4 ping
-// More information @https://pkg.go.dev/github.com/go-ping/ping
-func enablePing() error {
-	return sysctl.Set("net.ipv4.ping_group_range", "0 2147483647")
+func checkAndFetch(dataSourceURL string) *MirrorsOnline {
+	// Check privileged
+	if os.Geteuid() != 0 {
+		colorful.Red("不支持非root用户使用,请以root权限运行")
+		os.Exit(0)
+	}
+	// set ping privileged
+	// Check update and fetch mirror source
+	mirrorSource, err := fetchData(dataSourceURL)
+	if err != nil {
+		colorful.Red("无法从远程获取镜像数据:%s", err.Error())
+		os.Exit(0)
+	}
+	return mirrorSource
+}
+
+func displayRefresh(buf *bufio.Reader) {
+	for {
+		line, err := buf.ReadString('\n')
+		if err != nil {
+			break
+		} else {
+			fmt.Print(line)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -6,8 +6,13 @@ require (
 	github.com/elastic/go-sysinfo v1.6.0
 	github.com/elastic/go-windows v1.0.1 // indirect
 	github.com/fatih/color v1.10.0
+	github.com/go-ping/ping v0.0.0-20210407214646-e4e642a95741
+	github.com/jedib0t/go-pretty/v6 v6.1.1
+	github.com/lorenzosaino/go-sysctl v0.1.1
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/procfs v0.6.0 // indirect
+	github.com/urfave/cli/v2 v2.3.0
+	golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1 // indirect
 	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
 	howett.net/plist v0.0.0-20201203080718-1454fab16a06 // indirect
 )
